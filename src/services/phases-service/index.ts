acec2169571b6a13a98454b31a497f9f312674bb
Mateus Macedo <mateusamacedo5@gmail.com>
2023-04-15 01:11:13 -0300

    feat: correction in phases-service

--- src/services/phases-service/index.ts
+++ src/services/phases-service/index.ts
@@ -2,18 +2,20 @@ import userRepository from "@/repositories/users-repository";
 
 export async function phaseAuthorization(userId: number, phaseId: number){
   if (phaseId<0||isNaN(phaseId)){
-    throw 401
+    return false
   }
+
+  console.log(userId)
 
   const user = await userRepository.findUserById(userId)
 
   console.log(userId, phaseId)
   
   if (user.phase < phaseId) {
-    throw 401
+    return false
   }
 
-  return user
+  return true
 }
 
 const phasesService = {
